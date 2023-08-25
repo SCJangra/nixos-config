196c0a9b58a856b73760fc665d56baf283c0ca55
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-08-25 09:36:03 +0530

    use neovim nightly

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, neovim-nightly-overlay, ... }:
 
 {
   imports =
@@ -87,6 +87,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [ neovim-nightly-overlay.overlay ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
--- flake.nix
+++ flake.nix
@@ -1,14 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system  = "x86_64-linux";
+      specialArgs = inputs;
       modules = [ 
         ./configuration.nix 
         home-manager.nixosModules.home-manager
