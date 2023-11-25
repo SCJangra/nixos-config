fec8629901e604fc0b7d7e2e3ceac6c9e59d6a93
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-11-25 15:45:23 +0530

    nixos: add parinfer-rust

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, parinfer-rust, ... }:
 
 {
   imports =
@@ -104,6 +104,7 @@
     vlc
     wget
     wl-clipboard
+    parinfer-rust
   ];
 
   # Fonts
--- flake.nix
+++ flake.nix
@@ -3,18 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    parinfer-rust-src = {
+      type = "github";
+      owner = "eraserhd";
+      repo = "parinfer-rust";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager }@inputs:
+  outputs = { self, nixpkgs, home-manager, parinfer-rust-src }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
+      };
+      parinfer-rust = pkgs.rustPlatform.buildRustPackage {
+        pname = "parinfer-rust";
+        src = parinfer-rust-src;
+        version = "1.0.0";
+        cargoLock.lockFile = "${parinfer-rust-src}/Cargo.lock";
       };
     in
     {
@@ -24,7 +38,7 @@
 
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs system; };
+          specialArgs = { inherit inputs system parinfer-rust; };
           modules = [ ./configuration.nix ];
         };
       };
