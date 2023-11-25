7b1a9a91aa3800bc74c702d6779235b89a70f383
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-11-25 09:47:59 +0530

    use nix flakes

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
+      inputs.home-manager.nixosModules.home-manager
     ];
 
   home-manager.users.scj       = import ./home-manager.nix;
@@ -77,9 +77,6 @@
 
   # Enable automatic login for the user.
   services.getty.autologinUser = "scj";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "SCJ's NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ nil ];
+      };
+
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs system; };
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+}
