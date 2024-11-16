ad345ec725dfe363a195b56e1c979a84fcb67880
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-11-16 11:20:51 +0530

    home-manager: add brave desktop entry

--- home-manager.nix
+++ home-manager.nix
@@ -72,6 +72,46 @@
   qt.enable             = true;
   qt.platformTheme.name = "qtct";
 
+  xdg.desktopEntries.brave-browser = let brave = "${pkgs.brave.outPath}/bin/brave"; in {
+    name = "Brave Web Browser";
+    genericName = "Web Browser";
+    comment = "Access the internet";
+    exec = "${brave} --ozone-platform-hint=auto --ozone-platform=wayland %U";
+    startupNotify = true;
+    terminal = false;
+    icon = "brave-browser";
+    type = "Application";
+    categories = [ "Network" "WebBrowser" ];
+    mimeType = [
+      "application/pdf"
+      "application/rdf+xml"
+      "application/rss+xml"
+      "application/xhtml+xml"
+      "application/xhtml_xml"
+      "application/xml"
+      "image/gif"
+      "image/jpeg"
+      "image/png"
+      "image/webp"
+      "text/html"
+      "text/xml"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https" 
+    ];
+
+    actions = {
+      new-window = {
+        name = "New Window";
+        exec = brave;
+      };
+
+      new-private-window = {
+        name = "New Incognito Window";
+        exec = "${brave} --incognito";
+      };
+    };
+  };
+
   home.sessionVariables = {
     EDITOR = "nvim";
 
